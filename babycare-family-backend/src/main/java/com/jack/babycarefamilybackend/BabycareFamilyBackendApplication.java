13464e0f9c5f3cfb639358d0e95d8d52b11749d9
youngsik won <42738747+HopesDad@users.noreply.github.com>
2025-04-26 18:15:11 +0900

    백엔드 생성

---
+++ babycare-family-backend/src/main/java/com/jack/babycarefamilybackend/BabycareFamilyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jack.babycarefamilybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabycareFamilyBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BabycareFamilyBackendApplication.class, args);
+  }
+
+}
---
+++ babycare-family-backend/src/test/java/com/jack/babycarefamilybackend/BabycareFamilyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.babycarefamilybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabycareFamilyBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
